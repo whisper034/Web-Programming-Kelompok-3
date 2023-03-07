7db05dc68625b070493c54f6844040066fe7ec19
Ariel Sefrian <ariel.sefrian@binus.ac.id>
2023-03-07 22:59:00 +0700

    #1 finished configuring tailwind css
    1. added logo for homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="text-center">
+<div class="h-screen">
+    <img src="{{ asset('/images/logo/WebProgLogo.png') }}" alt="WebProgLogo" class="mx-auto">
+</div>
 
+<div class="font-bold">
+    hello there.
+</div>
 </body>
+<script src="{{ asset('js/app.js') }}"></script>
 </html>
